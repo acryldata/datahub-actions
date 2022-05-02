10762fc233d44384f88894398dd8c3a11f950f9b
John Joyce <john@acryl.io>
2022-05-02 13:30:47 -0700

    Adding all licenses

--- datahub_actions/src/datahub_actions/api/action_graph.py
+++ datahub_actions/src/datahub_actions/api/action_graph.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# TODO: John Clean this up.
-
 import json
 import time
 import urllib.parse
