5eb991d00b7d8fd28706cc18634b45303c9ede60
Shirshanka Das <shirshanka@apache.org>
2022-07-25 08:50:54 -0700

    feat(test-connection): bumping acryl-executor, add test_connection wiring (#29)

--- datahub-actions/setup.py
+++ datahub-actions/setup.py
@@ -80,7 +80,7 @@ plugins: Dict[str, Set[str]] = {
     "kafka": kafka_common,
     # Action Plugins 
     "executor": { 
-        f"acryl-executor>=0.0.3rc2",
+        f"acryl-executor>=0.0.3rc3",
     }
     # Transformer Plugins (None yet)
 }
