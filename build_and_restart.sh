5a83fb3a069b5719b4f5912566d21833fd711c6c
Harshal Sheth <hsheth2@gmail.com>
2022-08-17 18:52:36 -0400

    update base version

--- build_and_restart.sh
+++ build_and_restart.sh
@@ -5,5 +5,5 @@ cd ../acryl-executor
 RELEASE_SKIP_UPLOAD=1 RELEASE_VERSION=0.0.4 ./scripts/release.sh
 cd -
 docker build .. -f docker/datahub-actions/Dockerfile -t acryldata/datahub-actions:debug
-DATAHUB_VERSION=57b7ade ACTIONS_VERSION=debug ../../datahub/metadata-ingestion/venv/bin/datahub docker quickstart
+DATAHUB_VERSION=6f58560 ACTIONS_VERSION=debug ../../datahub/metadata-ingestion/venv/bin/datahub docker quickstart
 
