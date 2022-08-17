08d985a5949eb574536f435710aab57227836602
Harshal Sheth <hsheth2@gmail.com>
2022-08-17 19:06:54 -0400

    remove f-string

--- datahub-actions/setup.py
+++ datahub-actions/setup.py
@@ -113,7 +113,7 @@ plugins: Dict[str, Set[str]] = {
     "kafka": kafka_common,
     # Action Plugins
     "executor": {
-        f"acryl-executor>=0.0.3",
+        "acryl-executor>=0.0.3",
     }
     # Transformer Plugins (None yet)
 }
