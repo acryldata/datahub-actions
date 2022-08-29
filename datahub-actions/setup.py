a84b59bdd93fb6ac0a06867afaf738379a11dc00
John Joyce <john@acryl.io>
2022-08-29 12:55:51 -0700

    Bumping acryl-executor

--- datahub-actions/setup.py
+++ datahub-actions/setup.py
@@ -113,7 +113,7 @@ plugins: Dict[str, Set[str]] = {
     "kafka": kafka_common,
     # Action Plugins
     "executor": {
-        "acryl-executor>=0.0.3.1",
+        "acryl-executor>=0.0.3.2",
     }
     # Transformer Plugins (None yet)
 }
