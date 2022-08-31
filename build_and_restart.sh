d8b6b349c337eabad00c6e089135acc0348ec29c
Harshal Sheth <hsheth2@gmail.com>
2022-08-31 16:31:00 -0700

    improvements to build and restart script

--- build_and_restart.sh
+++ build_and_restart.sh
@@ -2,8 +2,8 @@
 set -euo pipefail
 
 cd ../acryl-executor
-RELEASE_SKIP_UPLOAD=1 RELEASE_VERSION=0.0.4 ./scripts/release.sh
+RELEASE_SKIP_UPLOAD=1 RELEASE_VERSION=0.0.0.dev0 ./scripts/release.sh
 cd -
 docker build .. -f docker/datahub-actions/Dockerfile -t acryldata/datahub-actions:debug
-DATAHUB_VERSION=6f58560 ACTIONS_VERSION=debug ../../datahub/metadata-ingestion/venv/bin/datahub docker quickstart
+DATAHUB_VERSION=7cfae0d ACTIONS_VERSION=debug ../../datahub/metadata-ingestion/venv/bin/datahub docker quickstart
 
