2149dd06698db69aaf67294de636de0fd501cdaa
Harshal Sheth <hsheth2@gmail.com>
2022-09-07 17:23:35 -0700

    bump acryl-executor to 0.0.3.4

--- datahub-actions/setup.py
+++ datahub-actions/setup.py
@@ -114,7 +114,7 @@ plugins: Dict[str, Set[str]] = {
     "kafka": kafka_common,
     # Action Plugins
     "executor": {
-        "acryl-executor>=0.0.3.2",
+        "acryl-executor>=0.0.3.4",
     }
     # Transformer Plugins (None yet)
 }
