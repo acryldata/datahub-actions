6b474ff60ca4a4a44caaed5a572f5fc44cb574af
Harshal Sheth <hsheth2@gmail.com>
2022-10-04 16:27:43 -0700

    skip image pull step

--- build_and_restart.sh
+++ build_and_restart.sh
@@ -5,5 +5,5 @@ cd ../acryl-executor
 RELEASE_SKIP_UPLOAD=1 RELEASE_VERSION=0.0.0.dev0 ./scripts/release.sh
 cd -
 docker build .. -f docker/datahub-actions/Dockerfile -t acryldata/datahub-actions:debug
-DATAHUB_VERSION=5d61924 ACTIONS_VERSION=debug ../../datahub/metadata-ingestion/venv/bin/datahub docker quickstart
+DATAHUB_VERSION=head ACTIONS_VERSION=debug ../../datahub/metadata-ingestion/venv/bin/datahub docker quickstart --no-pull-images
 
