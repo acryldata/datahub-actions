792a8a8a7e33959ec3ebe95a3dde030438298f8a
Tim Costa <tim@timcosta.io>
2023-01-25 12:31:35 -0600

    feat(): allow overriding docker config yaml paths (#66)

--- docker/datahub-actions/start.sh
+++ docker/datahub-actions/start.sh
@@ -14,20 +14,23 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+SYS_CONFIGS_PATH="${DATAHUB_ACTIONS_SYSTEM_CONFIGS_PATH:-/etc/datahub/actions/system/conf}"
+USER_CONFIGS_PATH="${DATAHUB_ACTIONS_USER_CONFIGS_PATH:-/etc/datahub/actions/conf}"
+
 touch /tmp/datahub/logs/actions/actions.out
 
 # Deploy System Actions
-if [ "$(ls -A /etc/datahub/actions/system/conf/)" ]; then
+if [ "$(ls -A ${SYS_CONFIGS_PATH}/)" ]; then
     config_files=""
-    # .yml 
-    for file in /etc/datahub/actions/system/conf/*.yml;
+    # .yml
+    for file in ${SYS_CONFIGS_PATH}/*.yml;
     do
         if [ -f "$file" ]; then
             config_files+="-c $file "
         fi
     done
     #.yaml
-    for file in /etc/datahub/actions/system/conf/*.yaml;
+    for file in ${SYS_CONFIGS_PATH}/*.yaml;
     do
         if [ -f "$file" ]; then
             config_files+="-c $file "
@@ -38,16 +41,16 @@ else
 fi
 
 # Deploy User Actions
-if [ "$(ls -A /etc/datahub/actions/conf/)" ]; then
+if [ "$(ls -A ${USER_CONFIGS_PATH}/)" ]; then
     # .yml
-    for file in /etc/datahub/actions/conf/*.yml;
+    for file in ${USER_CONFIGS_PATH}/*.yml;
     do
         if [ -f "$file" ]; then
             config_files+="-c $file "
         fi
     done
     #.yaml
-    for file in /etc/datahub/actions/conf/*.yaml;
+    for file in ${USER_CONFIGS_PATH}/*.yaml;
     do
         if [ -f "$file" ]; then
             config_files+="-c $file "
