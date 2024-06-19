18c43dc72a3113bcd65b851de65d2476a142952a
Jeff Lewis <jeff.lewis@acryl.io>
2024-06-19 13:18:31 -0500

    chore: include specified aspects in metadata change sync action

--- datahub-actions/src/datahub_actions/plugin/action/metadata_change_sync/metadata_change_sync.py
+++ datahub-actions/src/datahub_actions/plugin/action/metadata_change_sync/metadata_change_sync.py
@@ -2,6 +2,8 @@ import json
 import logging
 from typing import Dict, List, Optional, Set, Union, cast
 
+import re
+
 from datahub.emitter.rest_emitter import DatahubRestEmitter
 from datahub.metadata.schema_classes import (
     MetadataChangeLogClass,
@@ -21,7 +23,9 @@ class MetadataChangeEmitterConfig(BaseModel):
     gms_server: Optional[str]
     gms_auth_token: Optional[str]
     aspects_to_exclude: Optional[List]
+    aspects_to_include: Optional[List]
     extra_headers: Optional[Dict[str, str]]
+    urn_regex: Optional[str]
 
 
 class MetadataChangeSyncAction(Action):
@@ -59,14 +63,17 @@ class MetadataChangeSyncAction(Action):
             if self.config.aspects_to_exclude
             else self.DEFAULT_ASPECTS_EXCLUDE_SET
         )
+        self.aspects_include_set = self.config.aspects_to_include
+        
         extra_headers_keys = (
             list(self.config.extra_headers.keys())
             if self.config.extra_headers
             else None
         )
         logger.info(
-            f"MetadataChangeSyncAction configured to emit mcp to gms server {self.config.gms_server} with extra headers {extra_headers_keys} and aspects to exclude {self.aspects_exclude_set}"
+            f"MetadataChangeSyncAction configured to emit mcp to gms server {self.config.gms_server} with extra headers {extra_headers_keys} and aspects to exclude {self.aspects_exclude_set} and aspects to include {self.aspects_include_set}"
         )
+        self.urn_regex = self.config.urn_regex
 
     def act(self, event: EventEnvelope) -> None:
         """
@@ -77,10 +84,20 @@ class MetadataChangeSyncAction(Action):
         if event.event_type is METADATA_CHANGE_LOG_EVENT_V1_TYPE:
             orig_event = cast(MetadataChangeLogClass, event.event)
             logger.debug(f"received orig_event {orig_event}")
-            if orig_event.get("aspectName") not in self.aspects_exclude_set:
+            regexUrn = self.urn_regex
+            urn_match = re.match(regexUrn, orig_event.entityUrn)
+            aspect_name = orig_event.get("aspectName")
+            logger.info(f"urn_match {urn_match} for entityUrn {orig_event.entityUrn}")
+            if (((self.aspects_include_set is not None and aspect_name in self.aspects_include_set)
+                or (self.aspects_include_set is None and aspect_name not in self.aspects_exclude_set))
+                and urn_match is not None):
                 mcp = self.buildMcp(orig_event)
+                
                 if mcp is not None:
+                    logger.debug(f"built mcp {mcp}")
                     self.emit(mcp)
+            else:
+                logger.debug(f"skip emitting mcp for aspect {orig_event.get('aspectName')} or entityUrn {orig_event.entityUrn}")
 
     def buildMcp(
         self, orig_event: MetadataChangeLogClass
