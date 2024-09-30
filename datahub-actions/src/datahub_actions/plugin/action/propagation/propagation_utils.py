c5af228542172eec3656767f6d40a9032c0d9182
Shirshanka Das <shirshanka@apache.org>
2024-09-29 20:13:37 -0700

    feat(propagation): add new capabilities and guardrails (#133)

--- datahub-actions/src/datahub_actions/plugin/action/propagation/propagation_utils.py
+++ datahub-actions/src/datahub_actions/plugin/action/propagation/propagation_utils.py
@@ -16,27 +16,33 @@ import json
 import time
 from abc import abstractmethod
 from enum import Enum
-from typing import Dict, Iterable, List, Optional, Tuple
+from functools import wraps
+from typing import Any, Dict, Iterable, List, Optional, Tuple
 
 import datahub.metadata.schema_classes as models
+from datahub.configuration.common import ConfigModel
 from datahub.emitter.mce_builder import make_schema_field_urn
-from datahub.ingestion.graph.client import SearchFilterRule
+from datahub.ingestion.graph.client import DataHubGraph, SearchFilterRule
 from datahub.metadata.schema_classes import MetadataAttributionClass
-from datahub.utilities.urns.urn import Urn
+from datahub.utilities.str_enum import StrEnum
+from datahub.utilities.urns.urn import Urn, guess_entity_type
+from pydantic import validator
 from pydantic.fields import Field
 from pydantic.main import BaseModel
+from ratelimit import limits, sleep_and_retry
 
 from datahub_actions.api.action_graph import AcrylDataHubGraph
 
 SYSTEM_ACTOR = "urn:li:corpuser:__datahub_system"
 
 
-class RelationshipType(Enum):
+class RelationshipType(StrEnum):
     LINEAGE = "lineage"  # signifies all types of lineage
     HIERARCHY = "hierarchy"  # signifies all types of hierarchy
+    SIBLING = "sibling"  # signifies all types of sibling
 
 
-class DirectionType(Enum):
+class DirectionType(StrEnum):
     UP = "up"  # signifies upstream or parent (depending on relationship type)
     DOWN = "down"  # signifies downstream or child (depending on relationship type)
     ALL = "all"  # signifies all directions
@@ -45,8 +51,7 @@ class DirectionType(Enum):
 class PropagationDirective(BaseModel):
     propagate: bool
     operation: str
-    relationship: RelationshipType = RelationshipType.LINEAGE
-    direction: DirectionType = DirectionType.UP
+    relationships: List[Tuple[RelationshipType, DirectionType]]
     entity: str = Field(
         description="Entity that currently triggered the propagation directive",
     )
@@ -61,6 +66,104 @@ class PropagationDirective(BaseModel):
         None,
         description="Actor that triggered the propagation through the original association.",
     )
+    propagation_started_at: Optional[int] = Field(
+        None,
+        description="Timestamp (in millis) when the original propagation event happened.",
+    )
+    propagation_depth: Optional[int] = Field(
+        default=0,
+        description="Depth of propagation. This is used to track the depth of the propagation.",
+    )
+
+
+class SourceDetails(BaseModel):
+    origin: Optional[str] = Field(
+        None,
+        description="Origin entity for the documentation. This is the entity that triggered the documentation propagation.",
+    )
+    via: Optional[str] = Field(
+        None,
+        description="Via entity for the documentation. This is the direct entity that the documentation was propagated through.",
+    )
+    propagated: Optional[str] = Field(
+        None,
+        description="Indicates whether the metadata element was propagated.",
+    )
+    actor: Optional[str] = Field(
+        None,
+        description="Actor that triggered the metadata propagation.",
+    )
+    propagation_started_at: Optional[int] = Field(
+        None,
+        description="Timestamp when the metadata propagation event happened.",
+    )
+    propagation_depth: Optional[int] = Field(
+        default=0,
+        description="Depth of metadata propagation.",
+    )
+    propagation_relationship: Optional[RelationshipType] = Field(
+        None,
+        description="The relationship that the metadata was propagated through.",
+    )
+    propagation_direction: Optional[DirectionType] = Field(
+        None,
+        description="The direction that the metadata was propagated through.",
+    )
+
+    @validator("propagated", pre=True)
+    def convert_boolean_to_lowercase_string(cls, v: Any) -> Optional[str]:
+        if isinstance(v, bool):
+            return str(v).lower()
+        return v
+
+    @validator("propagation_depth", "propagation_started_at", pre=True)
+    def convert_to_int(cls, v: Any) -> Optional[int]:
+        if v is not None:
+            return int(v)
+        return v
+
+    def for_metadata_attribution(self) -> Dict[str, str]:
+        """
+        Convert the SourceDetails object to a dictionary that can be used in
+        Metadata Attribution MCPs.
+        """
+        result = {}
+        for k, v in self.dict(exclude_none=True).items():
+            if isinstance(v, Enum):
+                result[k] = v.value  # Use the enum's value
+            elif isinstance(v, int):
+                result[k] = str(v)  # Convert int to string
+            else:
+                result[k] = str(v)  # Convert everything else to string
+        return result
+
+
+class PropagationConfig(ConfigModel):
+    """
+    Base class for all propagation configs
+    """
+
+    max_propagation_depth: int = 5
+    max_propagation_fanout: int = 1000
+    max_propagation_time_millis: int = 1000 * 60 * 60 * 1  # 1 hour
+    rate_limit_propagated_writes: int = 15000  # 15000 writes per 15 seconds (default)
+    rate_limit_propagated_writes_period: int = 15  # Every 15 seconds
+
+    def get_rate_limited_emit_mcp(self, emitter: DataHubGraph) -> Any:
+        """
+        Returns a rate limited emitter that can be used to emit metadata for propagation
+        """
+
+        @sleep_and_retry
+        @limits(
+            calls=self.rate_limit_propagated_writes,
+            period=self.rate_limit_propagated_writes_period,
+        )
+        @wraps(emitter.emit_mcp)
+        def wrapper(*args, **kwargs):
+            return emitter.emit_mcp(*args, **kwargs)
+
+        return wrapper
 
 
 def get_attribution_and_context_from_directive(
@@ -80,7 +183,20 @@ def get_attribution_and_context_from_directive(
     source_detail: dict[str, str] = {
         "origin": propagation_directive.origin,
         "propagated": "true",
+        "propagation_depth": str(propagation_directive.propagation_depth),
+        "propagation_started_at": str(
+            propagation_directive.propagation_started_at
+            if propagation_directive.propagation_started_at
+            else time
+        ),
     }
+    if propagation_directive.relationships:
+        source_detail["propagation_relationship"] = propagation_directive.relationships[
+            0
+        ][0].value
+        source_detail["propagation_direction"] = propagation_directive.relationships[0][
+            1
+        ].value
     if propagation_directive.actor:
         source_detail["actor"] = propagation_directive.actor
     else:
@@ -144,7 +260,7 @@ def get_unique_siblings(graph: AcrylDataHubGraph, entity_urn: str) -> list[str]:
     Get unique siblings for the entity urn
     """
 
-    if entity_urn.startswith("urn:li:schemaField"):
+    if guess_entity_type(entity_urn) == "schemaField":
         parent_urn = Urn.create_from_string(entity_urn).get_entity_id()[0]
         entity_field_path = Urn.create_from_string(entity_urn).get_entity_id()[1]
         # Does my parent have siblings?
@@ -158,7 +274,7 @@ def get_unique_siblings(graph: AcrylDataHubGraph, entity_urn: str) -> list[str]:
                 target_sibling = other_siblings[0]
                 # now we need to find the schema field in this sibling that
                 # matches us
-                if target_sibling.startswith("urn:li:dataset"):
+                if guess_entity_type(target_sibling) == "dataset":
                     schema_fields = graph.graph.get_aspect(
                         target_sibling, models.SchemaMetadataClass
                     )
