cf62ef5f7413876b50361e6caf27adeaf8e5612d
treff7es <treff7es@gmail.com>
2024-10-29 11:13:57 +0100

    Add way to filter by aspect_type

--- datahub-actions/src/datahub_actions/plugin/action/metadata_change_sync/metadata_change_sync.py
+++ datahub-actions/src/datahub_actions/plugin/action/metadata_change_sync/metadata_change_sync.py
@@ -7,7 +7,7 @@ from datahub.metadata.schema_classes import (
     MetadataChangeLogClass,
     MetadataChangeProposalClass,
 )
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from datahub_actions.action.action import Action
 from datahub_actions.event.event_envelope import EventEnvelope
@@ -21,7 +21,9 @@ class MetadataChangeEmitterConfig(BaseModel):
     gms_server: Optional[str]
     gms_auth_token: Optional[str]
     aspects_to_exclude: Optional[List]
+    entity_type_to_exclude: Optional[List] = Field(default_factory=list)
     extra_headers: Optional[Dict[str, str]]
+    enable_soft_delete_filter: Optional[bool] = True
 
 
 class MetadataChangeSyncAction(Action):
@@ -59,6 +61,7 @@ class MetadataChangeSyncAction(Action):
             if self.config.aspects_to_exclude
             else self.DEFAULT_ASPECTS_EXCLUDE_SET
         )
+
         extra_headers_keys = (
             list(self.config.extra_headers.keys())
             if self.config.extra_headers
@@ -77,7 +80,11 @@ class MetadataChangeSyncAction(Action):
         if event.event_type is METADATA_CHANGE_LOG_EVENT_V1_TYPE:
             orig_event = cast(MetadataChangeLogClass, event.event)
             logger.debug(f"received orig_event {orig_event}")
-            if orig_event.get("aspectName") not in self.aspects_exclude_set:
+            if (
+                orig_event.get("aspectName") not in self.aspects_exclude_set
+                and orig_event.get("entityType")
+                not in self.config.entity_type_to_exclude
+            ):
                 mcp = self.buildMcp(orig_event)
                 if mcp is not None:
                     self.emit(mcp)
