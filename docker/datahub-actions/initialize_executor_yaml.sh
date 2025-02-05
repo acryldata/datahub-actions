be1879faaed710c0abb5916d12060b0a4c63e539
jaegwon.seo <jaegwon.seo@toss.im>
2025-02-05 11:16:02 +0900

    .

--- docker/datahub-actions/initialize_executor_yaml.sh
+++ docker/datahub-actions/initialize_executor_yaml.sh
@@ -4,8 +4,6 @@ CONFIG_FILE="/etc/datahub/actions/system/conf/executor.yaml"
 
 # Copy the original template file to the actual configuration file
 cp /etc/datahub/actions/system/conf/executor.yaml.template $CONFIG_FILE
-
-# Convert consumer_config environment variables to YAML format
 CONSUMER_CONFIG=""
 for var in $(env | grep ^KAFKA_PROPERTIES_ | awk -F= '{print $1}'); do
   key=$(echo $var | sed 's/^KAFKA_PROPERTIES_//g' | tr '[:upper:]' '[:lower:]' | tr '_' '.')
@@ -13,10 +11,12 @@ for var in $(env | grep ^KAFKA_PROPERTIES_ | awk -F= '{print $1}'); do
   CONSUMER_CONFIG="${CONSUMER_CONFIG}\n      ${key}: \"${value}\""
 done
 
-# If consumer_config is not empty, insert it into the YAML file
+# If consumer_config is not empty, insert it *before* topic_routes
 if [ ! -z "$CONSUMER_CONFIG" ]; then
-  sed -i "/topic_routes:/a\ \ \ consumer_config: ${CONSUMER_CONFIG}" $CONFIG_FILE
+  sed -i "/topic_routes:/i\    consumer_config:" $CONFIG_FILE
+  sed -i "/consumer_config:/a\ ${CONSUMER_CONFIG}" $CONFIG_FILE
 fi
+
 
 # Execute the provided command
 exec "$@"
