d2390472a267a0f763bf5b962e5ac0db93672550
jaegwon.seo <jaegwon.seo@toss.im>
2025-02-05 12:30:40 +0900

    fix script

--- docker/datahub-actions/initialize_executor_yaml.sh
+++ docker/datahub-actions/initialize_executor_yaml.sh
@@ -8,12 +8,14 @@ CONSUMER_CONFIG=""
 for var in $(env | grep ^KAFKA_PROPERTIES_ | awk -F= '{print $1}'); do
   key=$(echo $var | sed 's/^KAFKA_PROPERTIES_//g' | tr '[:upper:]' '[:lower:]' | tr '_' '.')
   value=${!var}
-  CONSUMER_CONFIG="${CONSUMER_CONFIG}\n      ${key}: ${value}"
+  CONSUMER_CONFIG="${CONSUMER_CONFIG}\n        ${key}: ${value}"
 done
+
+CONSUMER_CONFIG=$(echo "$CONSUMER_CONFIG" | cut -c4-)
 
 # If consumer_config is not empty, insert it *before* topic_routes
 if [ ! -z "$CONSUMER_CONFIG" ]; then
-  sed -i "/topic_routes:/i\    consumer_config:" $CONFIG_FILE
+  sed -i "/topic_routes:/i\      consumer_config:" $CONFIG_FILE
   sed -i "/consumer_config:/a\ ${CONSUMER_CONFIG}" $CONFIG_FILE
 fi
 
